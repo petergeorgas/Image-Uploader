d7a4bc45bde25454935d3ab14bf1025350187a5e
Peter Georgas <peter_georgas@yahoo.com>
2021-10-14 18:57:45 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-container">
+      <div className="content-box">
+        <div className="flex-container-inner">
+          <h2 className="box-header">Upload your image</h2>
+          <div className="dashed-box"></div>
+        </div>
+      </div>
     </div>
   );
 }
